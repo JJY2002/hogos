2e15042546b74ab07903684e427791757c6ba621
alifirfan03 <53941669+alifirfan03@users.noreply.github.com>
2025-05-25 14:07:10 +0800

    edit table

--- database/migrations/2025_05_25_034344_create_order_items_table.php
+++ database/migrations/2025_05_25_034344_create_order_items_table.php
@@ -13,14 +13,14 @@ return new class extends Migration
 {
     Schema::create('order_items', function (Blueprint $table) {
         $table->id();
-        $table->unsignedBigInteger('order_id');
+        //$table->unsignedBigInteger('order_id');
         $table->string('menu_name');
         $table->integer('quantity');
         $table->decimal('price', 8, 2);
         $table->string('image')->nullable(); // if storing image path
         $table->timestamps();
-
-        $table->foreign('order_id')->references('id')->on('orders')->onDelete('cascade');
+       //$table->foreign('order_id')->references('id')->on('orders')->onDelete('cascade');
+        $table->foreignId('order_id')->constrained('orders')->onDelete('cascade');
     });
 }
 
