ef6af58aa52fc83413f5a94e3dcfee92a91420ad
alifirfan03 <53941669+alifirfan03@users.noreply.github.com>
2025-06-14 22:35:43 +0800

    ordered_items table

---
+++ database/migrations/2025_05_15_090850_ordered_items_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+{
+    Schema::create('ordered_items', function (Blueprint $table) {
+        $table->id();
+        $table->unsignedBigInteger('order_item_id');
+        $table->integer('tableNum');
+        $table->timestamps();
+
+        $table->foreign('order_item_id')->references('id')->on('order_items')->onDelete('cascade');
+    });
+}
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('menus');
+    }
+};
